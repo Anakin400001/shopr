4aa4d0fa0fc14dfa448d8bc98795a1723de5e730
Anakin400001 <58471681+Anakin400001@users.noreply.github.com>
2020-09-27 16:56:37 +0300

    Updates

--- precache-manifest.e71658a008ae93a698f82839d943e205.js
+++ precache-manifest.e71658a008ae93a698f82839d943e205.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "23660e590134d5c055842c0699103fbd",
+    "revision": "431518bbae423d3063cf1d22f9401723",
     "url": "./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.09e62520.chunk.css"
   },
   {
-    "revision": "8174a3c5831f94870b86",
+    "revision": "d73472c86722c86c1e0f",
     "url": "./static/css/main.d9069527.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.5561e31e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8174a3c5831f94870b86",
-    "url": "./static/js/main.24647771.chunk.js"
+    "revision": "d73472c86722c86c1e0f",
+    "url": "./static/js/main.d1dcb352.chunk.js"
   },
   {
     "revision": "368c18639139db16e3bb",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a02dc99e974a5387225147ad66b6bef7.js"
+  "./precache-manifest.e71658a008ae93a698f82839d943e205.js"
 );
 
 self.addEventListener('message', (event) => {
